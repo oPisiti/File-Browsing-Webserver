6f1e329768b69b41b84c1e662cc4dd57f8845c94
leah <laurohrfranca@gmail.com>
2024-12-18 01:21:10 -0300

    Refactoring, new Error enum and basic css to fs

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,4 +1,4 @@
-use std::{fs, io};
+use std::fs;
 
 pub struct RenderFlags{
     pub fs_path: String,
@@ -12,6 +12,12 @@ impl Default for RenderFlags{
     }
 }
 
+#[derive(Debug)]
+pub enum RenderError{
+    InvalidId(String),
+    FilesList
+}
+
 /// Will search for an identifier and replace it with specific data.
 /// Can be thought of as a rudimentary Jinja implementation.
 /// 
@@ -19,7 +25,8 @@ impl Default for RenderFlags{
 /// 
 /// Supported identifiers:
 /// - {{files_list}}
-pub fn render_index_page(page: &mut String, flags: &RenderFlags) -> Result<(), std::io::Error>{
+/// - {{up_level_link}}
+pub fn render_index_page(page: &mut String, flags: &RenderFlags) -> Result<(), RenderError>{
     
     // Supported identifiers must be added here and handled below
     let identifiers = ["{{files_list}}", "{{up_level_link}}"];
@@ -35,59 +42,67 @@ pub fn render_index_page(page: &mut String, flags: &RenderFlags) -> Result<(), s
         if tokens.len() < 2 {continue;}
 
         // Render and stitch back together
-        let mut list_html;
-        match id {
-            "{{files_list}}" => {
-                list_html = String::from("<ul>");
-
-                let entries = fs::read_dir(&flags.fs_path)?;
-                for entry in entries{
-                    let entry = entry?;
-
-                    list_html += "<li>";
-                    
-                    // File name
-                    if let Some(name) = entry.path().file_name(){
-                        let file_name = name.to_str().unwrap_or_default();
-                        if entry.file_type()?.is_dir(){
-                            
-                            // Determine the correct url to reference
-                            let mut href_path = String::from("/fs");
-                            if flags.fs_path != "/"{
-                                href_path.push_str(flags.fs_path.as_str());
-                            }
-                            href_path = format!("{href_path}/{file_name}");
-                            
-                            list_html += format!("<a href={href_path}>{file_name}</a>").as_str();
-                        }
-                        else{
-                            list_html += file_name;
-                        }
-                    }
-
-                    list_html += "</li>";
-                }
-
-                list_html += "</ul>";
-            },
-            "{{up_level_link}}" => {
-                list_html = String::from("<a href=/fs");
-
-                let fs_path = flags.fs_path
-                    .split("/")
-                    .collect::<Vec<&str>>();
-
-                list_html += &fs_path[..fs_path.len()-1].join("/");
-                
-                list_html += ">../</a>";
-            }
-            _ => {
-                return Err(io::Error::new(io::ErrorKind::NotFound, "Error rendering file"))
-            }
-        }
+        let list_html= match id {
+            "{{files_list}}" => render_files_list(&flags.fs_path).unwrap_or_default(),
+            "{{up_level_link}}" => render_up_level_link(&flags.fs_path).unwrap_or_default(),
+            _ => return Err(RenderError::InvalidId("Error rendering file".to_string()))
+        };
 
         *page = tokens.join(&list_html);
     }
 
     Ok(())
+}
+
+fn render_files_list(path: &str) -> Result<String, RenderError>{
+    let entries = fs::read_dir(&path)
+        .map_err(|_| RenderError::FilesList)?;
+
+    let mut output = String::from("<ul>");
+
+    for entry in entries{
+        let entry = entry.map_err(|_| RenderError::FilesList)?;
+
+        output += "<li>";
+        
+        // File name
+        if let Some(name) = entry.path().file_name(){
+            let file_name = name.to_str().unwrap_or_default();
+
+            let entry_file_type = entry.file_type();
+            if entry_file_type.is_err(){continue;}
+            if entry_file_type.unwrap().is_dir(){                
+                // Determine the correct url to reference
+                let mut href_path = String::from("/fs");
+                if path != "/"{
+                    href_path += path;
+                }
+                href_path = format!("{href_path}/{file_name}");
+                
+                output += format!("<a href={href_path}>{file_name}</a>").as_str();
+            }
+            else{
+                output += file_name;
+            }
+        }
+
+        output += "</li>";
+    }
+
+    output += "</ul>";
+    
+    Ok(output)
+}
+
+fn render_up_level_link(path: &str) -> Result<String, RenderError>{
+    let mut output = String::from("<a href=/fs");
+
+    let fs_path = path
+        .split("/")
+        .collect::<Vec<&str>>();
+
+    output += &fs_path[..fs_path.len()-1].join("/");
+    
+    output += ">../</a>";
+    Ok(output)
 }
