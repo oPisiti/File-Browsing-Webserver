f7061362ed3fa3079f78e8b532051a66c8fb222f
leah <laurohrfranca@gmail.com>
2025-01-21 23:21:36 -0300

    Multithreading!

--- src/main.rs
+++ src/main.rs
@@ -8,39 +8,56 @@ mod threads;
 
 // For convenience
 use requests::RequestResult;
+use threads::{ThreadPool, ThreadPoolError};
 
 fn main() {
-    const BIND_ADDR: &str = "127.0.0.1:7878";
-    let listener = TcpListener::bind(BIND_ADDR).unwrap();
+    const BIND_PORT: &str = "7878";
+    const BASE_PATH: &str = "/home/leah";
+    let listener = TcpListener::bind("127.0.0.1:".to_owned() + BIND_PORT).unwrap();
 
-    println!("Serving files on http://localhost:7878/fs");
+    println!("[INFO] {}", "Serving files on http://localhost:".to_owned() + BIND_PORT + "/fs");
 
-    let base_path = "/home/leah";
+    let pool_size: usize = 4;
+    let thread_pool = ThreadPool::build(pool_size);
+    if thread_pool.is_err(){
+        println!("[ERR] Could not create threadpool with {pool_size} threads. Aborting");
+    }
+    let thread_pool = thread_pool.unwrap();
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        let handle_result = handler::handle_connection(stream, base_path);
+        // Handle stream in threadpool
+        let stream_handle = thread_pool.execute(|| {
+            let handle_result = handler::handle_connection(stream, BASE_PATH);
 
-        // If the handling of the request fails, deal with it
-        if handle_result.is_ok() {
-            if let RequestResult::Ok(ok_msg) = handle_result.unwrap() {
-                println!("{ok_msg}")
-            }
-        } else {
-            match handle_result.unwrap_err() {
-                RequestResult::UnsupportedURI(uri) => {
-                    println!("URI '{uri}' is not currently supported")
+            // If the handling of the request fails, deal with it
+            if handle_result.is_ok() {
+                if let RequestResult::Ok(ok_msg) = handle_result.unwrap() {
+                    println!("[REQ] {ok_msg}")
                 }
-                RequestResult::InvalidRequest => println!("Invalid request"),
-                RequestResult::InvalidMethod => println!("HTTP method not supported"),
-                RequestResult::FileNotFound(err)
-                | RequestResult::RenderingError(err)
-                | RequestResult::StreamError(err) => println!("{err}"),
-                RequestResult::FilePathNotFound => {
-                    println!("Path specified in the url was not found")
+            } else {
+                print!("[ERR] ");
+                match handle_result.unwrap_err() {
+                    RequestResult::UnsupportedURI(uri) => {
+                        println!("URI '{uri}' is not currently supported")
+                    }
+                    RequestResult::InvalidRequest => println!("Invalid request"),
+                    RequestResult::InvalidMethod => println!("HTTP method not supported"),
+                    RequestResult::FileNotFound(err)
+                    | RequestResult::RenderingError(err)
+                    | RequestResult::StreamError(err) => println!("{err}"),
+                    RequestResult::FilePathNotFound => {
+                        println!("Path specified in the url was not found")
+                    }
+                    _ => println!("Request error"),
                 }
-                _ => (),
-            }
+            } 
+        });
+
+        // Deal with threadpool error
+        if let Err(ThreadPoolError::ClosureExecError(msg)) = stream_handle{
+            println!("[ERR] {msg}");
         }
     }
 }
--- src/threads.rs
+++ src/threads.rs
@@ -1,49 +1,65 @@
-// use std::thread;
+use std::{sync::{mpsc, Arc, Mutex}, thread};
 
-// pub enum PoolCreationError{
-//     InvalidSize,
-// }
+#[derive(Debug)]
+pub enum ThreadPoolError{
+    InvalidSize,
+    ClosureExecError(String)
+}
 
-// pub struct ThreadPool{
-//     threads: Vec<thread::JoinHandle<()>>
-// }
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
-// impl ThreadPool{
-//     // /// Create a new ThreadPool.
-//     // ///
-//     // /// The size is the number of threads in the pool.
-//     // ///
-//     // /// # Panics
-//     // ///
-//     // /// The `new` function will panic if the size is zero.
-//     // pub fn new(size: usize) -> ThreadPool{
-//     //     assert!(size > 0);
+pub struct ThreadPool{
+    sender: mpsc::Sender<Job>,
+    threads: Vec<thread::JoinHandle<Job>>
+}
 
-//     //     ThreadPool
-//     // }
 
-//     /// Builds a new ThreadPool.
-//     ///
-//     /// The size is the number of threads in the pool.
-//     ///
-//     /// This does not panic
-//     pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError>{
-//         if size < 1{
-//             return Err(PoolCreationError::InvalidSize);
-//         }
+impl ThreadPool{
+    /// Builds a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// This does not panic
+    pub fn build(size: usize) -> Result<ThreadPool, ThreadPoolError>{
+        if size < 1{
+            return Err(ThreadPoolError::InvalidSize);
+        }
 
-//         let mut threads = Vec::with_capacity(size);
+        let mut threads = Vec::with_capacity(size);
 
-//         for i in 0..size{
-//             // threads[i] = thread::spawn(f)
-//         }
+        // Create a channel and wrap receiver in mutex and arc.
+        // An arc clone will be passed to each new thread.
+        // This will have a single producer and multiple consumers
+        let (tx, rx) = mpsc::channel::<Job>();
+        let receiver = Arc::new(Mutex::new(rx));
 
-//         Ok(ThreadPool{threads})
-//     }
+        for i in 0..size{
+            let rec_clone = Arc::clone(&receiver);
+            threads.push(thread::spawn(move || loop {
+                    let mutex = rec_clone.lock();
+                    if mutex.is_err(){continue;}
+    
+                    let job = mutex.unwrap().recv().unwrap();
+    
+                    println!("[THREAD] Worker {i} got a job! Executing...");
+                    job();
+                }));           
+        }
 
-//     pub fn execute<F>(&self, f: F)
-//     where
-//         F: FnOnce() + Send + 'static,
-//     {
-//     }
-// }
+        Ok(ThreadPool{sender: tx, threads: threads})
+    }
+
+    /// Executes the given closure in a thread
+    ///  
+    pub fn execute<F>(&self, f: F) -> Result<(), ThreadPoolError>
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender.send(job)
+            .map_err(|_e|{
+                ThreadPoolError::ClosureExecError("Closure could not be send to a thread".to_string())
+            })
+    }
+}
